4d007ce0a876ab7f02b7eca1b0a6932729b646f2
siddhant <siddhantawathare6@gmail.com>
2026-04-13 14:02:03 +0530

    added profile concept

--- src/main/java/com/bank/loan/controller/LoanController.java
+++ src/main/java/com/bank/loan/controller/LoanController.java
@@ -2,6 +2,7 @@ package com.bank.loan.controller;
 
 import com.bank.loan.constant.LoanConstant;
 import com.bank.loan.dto.ErrorResponseDto;
+import com.bank.loan.dto.LoanContactInfoDto;
 import com.bank.loan.dto.LoanDto;
 import com.bank.loan.dto.ResponseDto;
 import com.bank.loan.service.ILoanService;
@@ -13,7 +14,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,10 +27,22 @@ import org.springframework.web.bind.annotation.*;
 )
 @RestController
 @RequestMapping("/api/loan")
-@RequiredArgsConstructor
 public class LoanController {
 
+    @Value("${build.version}")
+    private String buildVersion;
+
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private LoanContactInfoDto loanContactInfoDto;
+
     private final ILoanService loanService;
+
+    public LoanController(ILoanService loanService) {
+        this.loanService = loanService;
+    }
 
     @Operation(
             summary = "Create Loan REST API",
@@ -145,6 +160,75 @@ public class LoanController {
         } else {
             return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseDto(LoanConstant.STATUS_417, LoanConstant.MESSAGE_417_DELETE));
         }
+    }
+
+    @Operation(
+            summary = "Get version information",
+            description = "Get version information that is deployed in loan-ms"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @GetMapping("/version")
+    public ResponseEntity<String> getVersion(){
+        return ResponseEntity.status(HttpStatus.OK).body(buildVersion);
+    }
+
+    @Operation(
+            summary = "Get home-path",
+            description = "Get home-path information that is installed in loan-ms"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @GetMapping("/home-path")
+    public ResponseEntity<String> getHomePath(){
+        return ResponseEntity.status(HttpStatus.OK).body(environment.getProperty("home"));
+    }
+
+    @Operation(
+            summary = "Get contact information",
+            description = "Get contact information that can be reached out in case of any issue"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @GetMapping("/contact")
+    public ResponseEntity<LoanContactInfoDto> getContactInfo(){
+        return ResponseEntity.status(HttpStatus.OK).body(loanContactInfoDto);
     }
 
 }
